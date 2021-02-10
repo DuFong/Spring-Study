6ccba4be49b3876e2e245e34632721484888125f
이영재 <youngjewall@me.com>
2021-02-10 23:25:44 +0900

    JPA를 사용한 레포지토리 개발

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // Mapping 작업 (ORM -> Object Relational Mapping)
     private Long id;
     private String name;
 
